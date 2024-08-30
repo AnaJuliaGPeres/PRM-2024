f50216e1f0e96bd21008beccc8943c95a7ebff3e
Ana Julia G. Peres <126435035+AnaJuliaGPeres@users.noreply.github.com>
2024-08-29 22:01:27 -0300

    feat: Texto com o que foi feito no dia

---
+++ mater-play-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './app/componentes/Header'
+
+function App() {
+
+  return (
+    
+    <div className="wrapper">
+        <Header />
+        
+    </div>
+
+  )
+}
+
+export default App
---
+++ mater-play-frontend/src/app/componentes/Header/index.tsx
@@ -0,0 +1,25 @@
+import './style.css'
+
+function Header() {
+
+    return (
+        <header>
+            <div className="container">
+                <div className="toolbar">
+                    <h6>Mater Play </h6>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Todos</a></li>
+                            <li><a href="#"></a>Series</li>
+                            <li><a href="#"></a>Filmes</li>
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
